30083bd24c37ae4838c25005b84bb0896c53a7b7
semah hamdi <sameh.hamdi73@gmail.com>
2021-12-24 21:54:11 +0100

    move nvim and tmux directory to .config directory

---
+++ .config/nvim/lua/config.lua
@@ -0,0 +1,11 @@
+-- global config
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.number = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+
+-- colorscheme
+vim.cmd([[colorscheme gruvbox]])
+--vim.cmd[[colorscheme nord]]
