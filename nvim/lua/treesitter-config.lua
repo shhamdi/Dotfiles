c5330c9d0ac12646c530e1cb242d3ff857b3c63f
semah hamdi <sameh.hamdi73@gmail.com>
2021-12-24 21:56:46 +0100

    delete old files

--- nvim/lua/treesitter-config.lua
+++
@@ -1,8 +0,0 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"cpp", "c", "javascript", "html", "css", "lua"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`) --false by default
-  highlight = {
-    enable = true,              -- false will disable the whole extension
-    additional_vim_regex_highlighting = false,
-  },
-}
